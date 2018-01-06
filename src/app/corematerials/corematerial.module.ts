64d2d46e6c889bc3cfa09edc19240c30f944ceb2
Phonemyatt <phonemyatt@gmail.com>
2018-01-06 13:48:21 +0800

    add material.angular and replace materialize-css but we don't want to focus on too much styling before the application actuallly shape up

---
+++ src/app/corematerials/corematerial.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { MatCardModule, MatButtonModule }  from '@angular/material';
+
+@NgModule({
+    imports: [ 
+               MatCardModule , 
+               MatButtonModule 
+             ],
+    exports: [ 
+                MatCardModule, 
+                MatButtonModule 
+            ]
+})
+export class CoreMaterialModule {}
